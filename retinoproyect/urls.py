cbcb28a1d057bf755f9d1072381b6e39bca1c899
AlexandraCruz <alexandra.cruz1@utp.ac.pa>
2025-06-05 15:54:53 -0500

    Añadi el btn registrase una imagen mas y toque algo de backend

--- retinoproyect/urls.py
+++ retinoproyect/urls.py
@@ -21,8 +21,8 @@ from retinoapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.login_view, name='login'),
-     path('', views.dashboard_view, name='dashboard'),
-     path('home/', views.home_view, name='home'),
+    path('', views.dashboard_view, name='dashboard'),
+    path('home/', views.home_view, name='home'),
 ]
 
 
