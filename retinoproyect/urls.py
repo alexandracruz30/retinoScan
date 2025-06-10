111eb2ab0d2eb45d93672ed296625b0fd37cf212
AlexandraCruz <alexandra.cruz1@utp.ac.pa>
2025-06-10 16:14:49 -0500

    ajustes de react

--- retinoproyect/urls.py
+++ retinoproyect/urls.py
@@ -22,8 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),  # <-- Agrega esta línea
-    path('', views.dashboard_view, name='dashboard'),
-    path('home/', views.home_view, name='home'),
+    path('', views.home_view, name='home'),
 ]
 
 
