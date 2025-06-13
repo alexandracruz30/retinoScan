deef8d90bfbb2d57ae7788bf4a215fa26c12d07a
AlexandraCruz <alexandra.cruz1@utp.ac.pa>
2025-06-13 16:07:10 -0500

    patien register quedo listo, hay que ver lo de la foto, y ver lo de patient detail la lista de detecciones de cada paciente

--- retinoapp/models.py
+++ retinoapp/models.py
@@ -3,14 +3,40 @@ from django.db import models
 # Create your models here. esto es para crear los modelos de la base de datos
 #Tabla para los datos del paciente
 class Paciente(models.Model):
+    # Datos Personales
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     cedula = models.CharField(max_length=20, unique=True)
     fecha_nacimiento = models.DateField()
-    email = models.EmailField(unique=True, null=True, blank=True)  # Opcional si quieres
+    foto = models.ImageField(upload_to='pacientes/', null=True, blank=True)
+    
+    # Datos Médicos Iniciales
+    GRUPOS_SANGUINEOS = [
+        ('A+', 'A+'),
+        ('A-', 'A-'),
+        ('B+', 'B+'),
+        ('B-', 'B-'),
+        ('AB+', 'AB+'),
+        ('AB-', 'AB-'),
+        ('O+', 'O+'),
+        ('O-', 'O-'),
+    ]
+    
+    grupo_sanguineo = models.CharField(
+        max_length=3,
+        choices=GRUPOS_SANGUINEOS,
+        null=True,
+        blank=True
+    )
+    alergias = models.TextField(null=True, blank=True)
+    enfermedades_cronicas = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
+
+    class Meta:
+        verbose_name = 'Paciente'
+        verbose_name_plural = 'Pacientes'
 #Tabla para las imagenes de retina
 class ImagenRetina(models.Model):
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name='imagenes')
